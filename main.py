0c37bfcf7cc090fe5302fed80ae27946cd849067
ArsenicO7 <zacky2500@gmail.com>
2022-04-05 20:41:17 -0400

    Another Test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ detector = Detector(model_type="OD")
 
 detector.onImage("/home/arsenic/Documents/complete_detectron2/images/3.jpg")
 
+#test comment
 #for x in range(1 , 5):
     #detector.onImage("home/arsenic/Documents/complete_detectron2/images/{}.jpg".format(x))
     #print (detector.list[dict])
