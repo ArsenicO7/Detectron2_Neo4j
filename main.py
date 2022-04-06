0f3879ca1517c02ec2746da65c288d62e7c7448c
ArsenicO7 <zacky2500@gmail.com>
2022-04-06 13:15:10 -0400

    ANOTHER TEST COMMIT

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ detector = Detector(model_type="OD")
 detector.onImage("/home/arsenic/Documents/complete_detectron2/images/3.jpg")
 
 #test comment
+print("Testing testing")
 #for x in range(1 , 5):
     #detector.onImage("home/arsenic/Documents/complete_detectron2/images/{}.jpg".format(x))
     #print (detector.list[dict])
